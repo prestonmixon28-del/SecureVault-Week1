9132d5215f832ed75bb2e70b1f874cf7ebf4542d
prestonmixon28-del <prestonmixon28@gmail.com>
2026-08-16 08:42:43 -0500

    Phase 1 finished

--- Journal.md
+++ Journal.md
@@ -1,2 +1,5 @@
 # Journal
-Write your Journal questions and notes here.
+Phase-1:
+For itemValue and itemName is because the data should not be changed from outside the class. Then I used public for constructor and toString methods so other classes can create and access the VaultItem safely. Using private attributes provides the better facts and help prevent accidental changes to objects data.
+Phase-2: 
+
---
+++ src/com/securevault/core/VaultItem.java
@@ -0,0 +1,24 @@
+package com.securevault.core;
+
+public class VaultItem {
+
+    private String itemName;
+    private double itemValue;
+
+    public VaultItem(String itemName, double itemValue) {
+        this.itemName = itemName;
+        this.itemValue = itemValue;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemValue() {
+        return  itemValue;
+    }
+    @Override
+    public String toString() {
+        return String.format("Item, %, value: $%.2f", itemName,itemValue);
+    }
+}
